295e42575c4bada0b1a6571d651cf5f8494f7c95
Avi Weinstock <aweinstock314@gmail.com>
2014-10-12 20:38:27 -0400

    Added some helpers for examining objects via reflection to repl_helpers.scm.

--- project/repl_helpers.scm
+++ project/repl_helpers.scm
@@ -1,1 +1,3 @@
 (define (startServer) (edu.rpi.csci.sdd.epic.webserver.WebServer:main (String[])))
+(define methodsOf java.lang.Class:getDeclaredMethods)
+(define fieldsOf java.lang.Class:getDeclaredFields)
