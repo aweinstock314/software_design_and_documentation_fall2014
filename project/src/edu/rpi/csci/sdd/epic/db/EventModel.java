4c8e6ef357018bb835a04a7ea0ff21182557f02a
Avi Weinstock <aweinstock314@gmail.com>
2014-10-26 19:06:51 -0400

    Added event name to JS serialization, and fixed silly oversight-bug involving casting event location to a boolean.

--- project/src/edu/rpi/csci/sdd/epic/db/EventModel.java
+++ project/src/edu/rpi/csci/sdd/epic/db/EventModel.java
@@ -27,20 +27,21 @@ public class EventModel
     {
         ArrayList<String> results = new ArrayList();
         Connection db = Util.getCredentialedDataSource().getConnection();
-        ResultSet rs = db.createStatement().executeQuery("SELECT id, host, source, creator, recurring, starttime, endtime, location, on_campus FROM events");
+        ResultSet rs = db.createStatement().executeQuery("SELECT name, id, host, source, creator, recurring, starttime, endtime, location, on_campus FROM events");
         while(rs.next())
         {
-            int id = rs.getInt(1);
-            String host = rs.getString(2);
-            String source = rs.getString(3);
-            String creator = rs.getString(4);
-            boolean recurring = rs.getBoolean(5);
-            long starttime = rs.getDate(6).getTime();
-            long endtime = rs.getDate(7).getTime();
-            boolean location = rs.getBoolean(8);
-            boolean on_campus = rs.getBoolean(9);
+            String name = rs.getString(1);
+            int id = rs.getInt(2);
+            String host = rs.getString(3);
+            String source = rs.getString(4);
+            String creator = rs.getString(5);
+            boolean recurring = rs.getBoolean(6);
+            long starttime = rs.getDate(7).getTime();
+            long endtime = rs.getDate(8).getTime();
+            String location = rs.getString(9);
+            boolean on_campus = rs.getBoolean(10);
             String tagsJSArray = "[]"; // TODO: get tags from database
-            results.add(String.format("{id: %d, host: \"%s\", source: \"%s\", creator: \"%s\", recurring: %b, starttime: %d, endtime: %d, location: \"%s\", on_campus: %b, tags: %s}", id, host, source, creator, recurring, starttime, endtime, location, on_campus, tagsJSArray));
+            results.add(String.format("{name: \"%s\", id: %d, host: \"%s\", source: \"%s\", creator: \"%s\", recurring: %b, starttime: %d, endtime: %d, location: \"%s\", on_campus: %b, tags: %s}", name, id, host, source, creator, recurring, starttime, endtime, location, on_campus, tagsJSArray));
         }
         return "[" + Util.joinList(results, ", ") + "]";
     }
