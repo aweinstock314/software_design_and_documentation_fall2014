96bf08170610b3978d252d4c60686f001b2a87ee
Avi Weinstock <aweinstock314@gmail.com>
2014-10-26 15:24:45 -0400

    Added debug statements to PostReqestProcessor.

--- project/src/edu/rpi/csci/sdd/epic/webserver/PostRequestProcessor.java
+++ project/src/edu/rpi/csci/sdd/epic/webserver/PostRequestProcessor.java
@@ -11,6 +11,7 @@ import edu.rpi.csci.sdd.epic.util.Util;
 
 public abstract class PostRequestProcessor implements HttpHandler
 {
+    private static final boolean DEBUG_MODE = true;
     protected abstract String processPostRequest(Map<String, String> postPairs) throws Exception;
 
     @Override
@@ -18,10 +19,12 @@ public abstract class PostRequestProcessor implements HttpHandler
     {
         Map<String, String> postPairs = new TreeMap<String, String>();
         String postData = Util.slurpStream(e.getRequestBody());
+        if(DEBUG_MODE) { System.out.printf("Post request for page \"%s\" (from \"%s\")\n", e.getRequestURI(), e.getRemoteAddress()); }
         for(String kvp : postData.split("&"))
         {
             String[] kv = kvp.split("=");
             postPairs.put(kv[0], kv[1]);
+            if(DEBUG_MODE) { System.out.printf("\t%s: %s\n", kv[0], kv[1]); }
         }
         try
         {
