a407235999a50d0ee3fa35561ad21d1f6254201e
Avi Weinstock <aweinstock314@gmail.com>
2014-10-27 14:55:11 -0400

    Fixed JS attempting to display events from weeks other than the one displayed. Fixed some DB connection leaks in EventModel that were causing the JS to fail after 3-4 refreshes. Added local version of jQuery to web folder (to ensure that the server works without an internet connection, for debugging).

--- project/src/edu/rpi/csci/sdd/epic/db/EventModel.java
+++ project/src/edu/rpi/csci/sdd/epic/db/EventModel.java
@@ -16,45 +16,57 @@ public class EventModel
     public static void createEvent(String name, String host, String source, String creator, boolean recurring, long starttime, long endtime, String location, boolean on_campus) throws SQLException
     {
         Connection db = Util.getCredentialedDataSource().getConnection();
-        Statement stmt = db.createStatement();
-		location = location.replace("\'","");
-        String sql = String.format("INSERT INTO events (name, host, source, creator, recurring, starttime, endtime, location, on_campus) VALUES ('%s', '%s', '%s', '%s', '%b', %s, %s, '%s', '%b')", name, host, source, creator, recurring, unixTimestamp(starttime), unixTimestamp(endtime), location, on_campus);
-		//sql = sql.replace("\'", "");
-		System.out.println(sql);
-        stmt.execute(sql);
+        try
+        {
+            Statement stmt = db.createStatement();
+            location = location.replace("\'","");
+            String sql = String.format("INSERT INTO events (name, host, source, creator, recurring, starttime, endtime, location, on_campus) VALUES ('%s', '%s', '%s', '%s', '%b', %s, %s, '%s', '%b')", name, host, source, creator, recurring, unixTimestamp(starttime), unixTimestamp(endtime), location, on_campus);
+            //sql = sql.replace("\'", "");
+            System.out.println(sql);
+            stmt.execute(sql);
+        }
+        finally { db.close(); }
     }
     public static String getEventsAsJSArray() throws SQLException
     {
         ArrayList<String> results = new ArrayList();
         Connection db = Util.getCredentialedDataSource().getConnection();
-        ResultSet rs = db.createStatement().executeQuery("SELECT name, id, host, source, creator, recurring, starttime, endtime, location, on_campus FROM events");
-        while(rs.next())
+        try
         {
-            String name = rs.getString(1);
-            int id = rs.getInt(2);
-            String host = rs.getString(3);
-            String source = rs.getString(4);
-            String creator = rs.getString(5);
-            boolean recurring = rs.getBoolean(6);
-            long starttime = rs.getDate(7).getTime();
-            long endtime = rs.getDate(8).getTime();
-            String location = rs.getString(9);
-            boolean on_campus = rs.getBoolean(10);
-            String tagsJSArray = getTagsArray(id);
-            results.add(String.format("{name: \"%s\", id: %d, host: \"%s\", source: \"%s\", creator: \"%s\", recurring: %b, starttime: %d, endtime: %d, location: \"%s\", on_campus: %b, tags: %s}", name, id, host, source, creator, recurring, starttime, endtime, location, on_campus, tagsJSArray));
+            ResultSet rs = db.createStatement().executeQuery("SELECT name, id, host, source, creator, recurring, starttime, endtime, location, on_campus FROM events");
+            while(rs.next())
+            {
+                String name = rs.getString(1);
+                int id = rs.getInt(2);
+                String host = rs.getString(3);
+                String source = rs.getString(4);
+                String creator = rs.getString(5);
+                boolean recurring = rs.getBoolean(6);
+                long starttime = rs.getDate(7).getTime();
+                long endtime = rs.getDate(8).getTime();
+                String location = rs.getString(9);
+                boolean on_campus = rs.getBoolean(10);
+                String tagsJSArray = getTagsArray(id);
+                results.add(String.format("{name: \"%s\", id: %d, host: \"%s\", source: \"%s\", creator: \"%s\", recurring: %b, starttime: %d, endtime: %d, location: \"%s\", on_campus: %b, tags: %s}", name, id, host, source, creator, recurring, starttime, endtime, location, on_campus, tagsJSArray));
+            }
         }
+        finally { db.close(); }
         return "[" + Util.joinList(results, ", ") + "]";
     }
     public static String getTagsArray(int eventId) throws SQLException
     {
         ArrayList<String> results = new ArrayList();
         Connection db = Util.getCredentialedDataSource().getConnection();
-        ResultSet rs = db.createStatement().executeQuery(String.format("SELECT tag FROM event_tags WHERE id='%d'", eventId));
-        while(rs.next())
+        try
         {
-            String tag = rs.getString(1);
-            results.add(tag);
+            ResultSet rs = db.createStatement().executeQuery(String.format("SELECT tag FROM event_tags WHERE id='%d'", eventId));
+            while(rs.next())
+            {
+                String tag = rs.getString(1);
+                results.add(tag);
+            }
         }
+        finally { db.close(); }
         return "[" + Util.joinList(results, ", ") + "]";
     }
 }
