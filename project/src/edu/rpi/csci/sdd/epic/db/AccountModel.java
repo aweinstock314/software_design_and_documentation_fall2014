0a357a2fdb26a3224220f8ff06a366ede917d01f
Avi Weinstock <aweinstock314@gmail.com>
2014-10-28 08:26:51 -0400

    Fixed a DB Connection leak in AccountModel.

--- project/src/edu/rpi/csci/sdd/epic/db/AccountModel.java
+++ project/src/edu/rpi/csci/sdd/epic/db/AccountModel.java
@@ -10,7 +10,11 @@ public class AccountModel
     public static void createAccount(String id, boolean event_provider, String email, String username, String password) throws SQLException
     {
         Connection db = Util.getCredentialedDataSource().getConnection();
-        Statement stmt = db.createStatement();
-        stmt.execute(String.format("INSERT INTO users (id, event_provider, email_address, username, password) VALUES ( '%s', '%b', '%s', '%s', '%s' )", id, event_provider, email, username, password));
+        try
+        {
+            Statement stmt = db.createStatement();
+            stmt.execute(String.format("INSERT INTO users (id, event_provider, email_address, username, password) VALUES ( '%s', '%b', '%s', '%s', '%s' )", id, event_provider, email, username, password));
+        }
+        finally { db.close(); }
     }
 }
