b57650f0b73c40c64f793542050d28caad23570d
Avi Weinstock <aweinstock314@gmail.com>
2014-11-01 22:16:46 -0400

    Commented GenericModel.

--- project/src/edu/rpi/csci/sdd/epic/db/GenericModel.java
+++ project/src/edu/rpi/csci/sdd/epic/db/GenericModel.java
@@ -27,36 +27,54 @@ public class GenericModel
             stmt.setObject(i, o);
         }
     }
+    // Insert some data into the database. ds is the DataSource 
+    //  used to connect, tablename is the name of the table 
+    //  to insert into, and vals is a map of (columname, 
+    //  value-to-insert) pairs.
     public static void insert(DataSource ds, String tablename, Map<String, Object> vals) throws SQLException
     {
+        // connect to the database
         Connection db = ds.getConnection();
         try
         {
+            // construct the SQL for the prepared statement
             String colnames = Util.joinIterable(vals.keySet(), ", ", "(", ")");
             String questionmarks = Util.joinIterable(new FiniteCyclicIterable("?", vals.keySet().size()), ", ", "(", ")");
             PreparedStatement stmt = db.prepareStatement(String.format("INSERT INTO %s %s VALUES %s", tablename, colnames, questionmarks));
+            // add the values to the prepared statement
             setPreparedStatementValues(stmt, vals);
+            // execute the insertion
             stmt.execute();
         }
+        // close the database connection to avoid a leak
         finally { db.close(); }
     }
+    // Read data from the database. ds is the DataSource used 
+    //  to connect, tablename is the name of the table to read 
+    //  from, and columns are the names of the columns to read. 
     public static Map<String, List<Object>> select(DataSource ds, String tablename, String... columns) throws SQLException
     {
+        // allocate/initialize the return value
         Map<String, List<Object>> ret = new LinkedHashMap<String, List<Object>>();
         for(String col : columns) { ret.put(col, new ArrayList<Object>()); }
+        // connect to the database
         Connection db = ds.getConnection();
         try
         {
             String colnames = Util.joinIterable(Arrays.asList(columns), ", ");
+            // execute the query
             ResultSet rs = db.createStatement().executeQuery(String.format("SELECT %s FROM %s", colnames, tablename));
+            // loop over the rows returned
             while(rs.next())
             {
                 for(int i=0; i<columns.length; i++)
                 {
+                    // add each row of data to the result object
                     ret.get(columns[i]).add(rs.getObject(i+1));
                 }
             }
         }
+        // close the database connection to avoid a leak
         finally { db.close(); }
         return ret;
     }
