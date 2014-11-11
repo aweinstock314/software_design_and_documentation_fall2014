f2452cb824551a285d312e7b488b5b179a005610
Avi Weinstock <aweinstock314@gmail.com>
2014-11-11 15:31:50 -0500

    Added check to prevent multiple users with the same username from being created in the database.

--- project/src/edu/rpi/csci/sdd/epic/db/AccountModel.java
+++ project/src/edu/rpi/csci/sdd/epic/db/AccountModel.java
@@ -11,6 +11,20 @@ import edu.rpi.csci.sdd.epic.util.Util;
 
 public class AccountModel
 {
+    // userExists(ds, username) checks to see if the user 
+    //  named by username already exists in the database 
+    //  represented by ds
+    public static boolean userExists(DataSource ds, String username) throws SQLException
+    {
+        Connection db = ds.getConnection();
+        try
+        {
+            PreparedStatement ps = db.prepareStatement("SELECT username FROM users WHERE username = ?");
+            ps.setString(1, username);
+            return ps.executeQuery().next();
+        }
+        finally { db.close(); }
+    }
     // createAccount, given account parameters, calls 
     //  GenericModel.insert to add account to "users" table in 
     //  database.  Values extracted from javascript create account 
@@ -19,15 +33,24 @@ public class AccountModel
     //  primarily in testing.
     public static void createAccount(String id, boolean event_provider, String email, String username, String password) throws SQLException
     {
-        // set up argument for GenericModel.insert
-        Map<String, Object> vals = new LinkedHashMap<String, Object>();
-        vals.put("id", id);
-        vals.put("event_provider", event_provider);
-        vals.put("email_address", email);
-        vals.put("username", username);
-        vals.put("password", password);
-        // insert the account data into the database
-        GenericModel.insert(DBUtil.getCredentialedDataSource(), "users", vals);
+        DataSource ds = DBUtil.getCredentialedDataSource();
+        if(!userExists(ds, username))
+        {
+            // set up argument for GenericModel.insert
+            Map<String, Object> vals = new LinkedHashMap<String, Object>();
+            vals.put("id", id);
+            vals.put("event_provider", event_provider);
+            vals.put("email_address", email);
+            vals.put("username", username);
+            vals.put("password", password);
+            // insert the account data into the database
+            GenericModel.insert(ds, "users", vals);
+        }
+        else
+        {
+            // TODO: more elegant return mechanism?
+            throw new RuntimeException(String.format("User \"%s\" already exists in the database.", username));
+        }
     }
     // getUsersTable() returns all values in the users table. Used primarily in testing.
     public static ArrayList<String> getUsersTable() throws SQLException
