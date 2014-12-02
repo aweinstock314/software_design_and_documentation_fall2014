c425462322c4687cdb7a94566872114e8386d5bf
Avi Weinstock <aweinstock314@gmail.com>
2014-12-02 14:54:44 -0500

    The create event form now displays success/failure messages, and the create event endpoint treats non-numeric strings for the dates as errors.

--- project/src/edu/rpi/csci/sdd/epic/webserver/CreateEvent.java
+++ project/src/edu/rpi/csci/sdd/epic/webserver/CreateEvent.java
@@ -8,19 +8,27 @@ import edu.rpi.csci.sdd.epic.db.EventModel;
 public class CreateEvent extends PostRequestProcessor
 {
     // Creates stringified success page with message indicating the event was created.
-    protected static final String successPage =
-        "<html><head><title>Event created</title></head><body>Event created successfully.</body></html>";
+    protected static final String successPage = "{\"success\": true}";
+    protected static final String failurePage = "{\"success\": false}";
 
     protected String processPostRequest(Map<String, String> postPairs) throws Exception
     {
         // extract parameters from the provided map
-		String name = postPairs.get("name");
+        String name = postPairs.get("name");
         String host = postPairs.get("host");
         String source = postPairs.get("source");
         String creator = "not yet implemented";
         boolean recurring = parseCheckbox(postPairs.get("recurring"));
-        long starttime = Long.valueOf(postPairs.get("starttime"));
-        long endtime = Long.valueOf(postPairs.get("endtime"));
+        long starttime, endtime;
+        try
+        {
+            starttime = Long.valueOf(postPairs.get("starttime"));
+            endtime = Long.valueOf(postPairs.get("endtime"));
+        }
+        catch(NumberFormatException nfe)
+        {
+            return failurePage;
+        }
         String location = postPairs.get("location");
         boolean oncampus = parseCheckbox(postPairs.get("oncampus"));
         // create the event
